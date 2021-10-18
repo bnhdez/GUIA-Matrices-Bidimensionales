4c466fa3bee0cd7bd6b4c1fff7c9e795501dafd7
Boris Hernández <borisnandez93@gmail.com>
2021-10-18 15:43:26 -0600

    cambios

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -6,7 +6,29 @@ namespace Ejercicio_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[,] control = new double[4, 7];
+            //cargar y visualizar una matriz
+            for (int tipo_clase = 0; tipo_clase < 4; tipo_clase++) //for para filas
+            {
+                for (int semanas = 0; semanas < 7; semanas++) // for interno para columnas
+                {
+                    Console.Write("Ingrese el valor de la posición [" + tipo_clase + "," + semanas + "]: ");
+                    control[tipo_clase, semanas] = int.Parse(Console.ReadLine());
+
+                }
+            }
+
+            for (int tipo_clase = 0; tipo_clase < 4; tipo_clase++)
+            {
+                Console.Write("\n");
+                for (int semanas = 0; semanas < 7; semanas++)
+                {
+                    Console.Write("\t" + control[tipo_clase, semanas] + "\t");
+                }
+            }
+
+
+
         }
     }
 }
