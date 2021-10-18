371ad64f14fee9b08d9aced2adbfc19b5533decd
Boris Hernández <borisnandez93@gmail.com>
2021-10-18 16:13:30 -0600

    cambios

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -9,6 +9,9 @@ namespace Ejercicio_3
             double[,] control = new double[3,4];
 
             Console.WriteLine("Ingrese los valores segun corresponde a la semana y el tipo de asistencia correspondiente");
+            Console.WriteLine("Tipo de asistencia 1: SOLO VIRTUAL");
+            Console.WriteLine("Tipo de asistencia 2: SOLO PRESENCIAL");
+            Console.WriteLine("Tipo de asistencia 3: HIBRIDA\n");
             //cargar y visualizar una matriz
             for (int tipo_clase = 0; tipo_clase < 3; tipo_clase++) //for para filas
             {
@@ -16,7 +19,7 @@ namespace Ejercicio_3
                 for (int semanas = 0; semanas < 4; semanas++) // for interno para columnas
                 {
                     int num_semana = semanas + 1;
-                    Console.Write("Ingrese el valor de la posicion [" + asistencia + "," + num_semana + "]: ");
+                    Console.Write("Ingrese el numero de estudiantes correspodientes [Tipo de asistencia " + asistencia + ", Semana " + num_semana + "]: ");
                     control[tipo_clase, semanas] = int.Parse(Console.ReadLine());
 
                 }
