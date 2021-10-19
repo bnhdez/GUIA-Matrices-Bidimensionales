ce8dbeade2b11c82df2f8e9d28774324681b5716
Boris Hernández <borisnandez93@gmail.com>
2021-10-18 19:26:51 -0600

    cambios

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -51,11 +51,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 2
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[1, 0] == "virtual") && (control[1, 1] == "virtual") && (control[1, 2] == "virtual") && (control[1, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[1, 0] == "presencial") && (control[1, 1] == "presencial") && (control[1, 2] == "presencial") && (control[1, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
@@ -64,11 +64,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 3
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[2, 0] == "virtual") && (control[2, 1] == "virtual") && (control[2, 2] == "virtual") && (control[2, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[2, 0] == "presencial") && (control[2, 1] == "presencial") && (control[2, 2] == "presencial") && (control[2, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
@@ -77,11 +77,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 4
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[3, 0] == "virtual") && (control[3, 1] == "virtual") && (control[3, 2] == "virtual") && (control[3, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[3, 0] == "presencial") && (control[3, 1] == "presencial") && (control[3, 2] == "presencial") && (control[3, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
@@ -90,11 +90,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 5
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[4, 0] == "virtual") && (control[4, 1] == "virtual") && (control[4, 2] == "virtual") && (control[4, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[4, 0] == "presencial") && (control[4, 1] == "presencial") && (control[4, 2] == "presencial") && (control[4, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
@@ -103,11 +103,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 6
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[5, 0] == "virtual") && (control[5, 1] == "virtual") && (control[5, 2] == "virtual") && (control[5, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[5, 0] == "presencial") && (control[5, 1] == "presencial") && (control[5, 2] == "presencial") && (control[5, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
@@ -116,11 +116,11 @@ namespace Ejercicio_3
                 control_3 = control_3 + 1;
             }
             //estudiante 7
-            if ((control[0, 0] == "virtual") && (control[0, 1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
+            if ((control[6, 0] == "virtual") && (control[6, 1] == "virtual") && (control[6, 2] == "virtual") && (control[6, 3] == "virtual"))
             {
                 control_1 = control_1 + 1;
             }
-            else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
+            else if ((control[6, 0] == "presencial") && (control[6, 1] == "presencial") && (control[6, 2] == "presencial") && (control[6, 3] == "presencial"))
             {
                 control_2 = control_2 + 1;
             }
