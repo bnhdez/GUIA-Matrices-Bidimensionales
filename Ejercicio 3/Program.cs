45b494329fae477574008a19ec2213610dc6a368
Boris Hernández <borisnandez93@gmail.com>
2021-10-18 19:32:44 -0600

    cambios

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -36,102 +36,102 @@ namespace Ejercicio_3
             }
 
             //conteo de asistencias
-            int control_1 = 0, control_2 = 0, control_3 = 0;
+            int ctrl_vrtl = 0, ctrl_presencial = 0, ctrl_hibrid = 0;
             //estudiante 1
             if ((control[0,0] == "virtual") && (control[0,1] == "virtual") && (control[0, 2] == "virtual") && (control[0, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[0, 0] == "presencial") && (control[0, 1] == "presencial") && (control[0, 2] == "presencial") && (control[0, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 2
             if ((control[1, 0] == "virtual") && (control[1, 1] == "virtual") && (control[1, 2] == "virtual") && (control[1, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[1, 0] == "presencial") && (control[1, 1] == "presencial") && (control[1, 2] == "presencial") && (control[1, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 3
             if ((control[2, 0] == "virtual") && (control[2, 1] == "virtual") && (control[2, 2] == "virtual") && (control[2, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[2, 0] == "presencial") && (control[2, 1] == "presencial") && (control[2, 2] == "presencial") && (control[2, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 4
             if ((control[3, 0] == "virtual") && (control[3, 1] == "virtual") && (control[3, 2] == "virtual") && (control[3, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[3, 0] == "presencial") && (control[3, 1] == "presencial") && (control[3, 2] == "presencial") && (control[3, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 5
             if ((control[4, 0] == "virtual") && (control[4, 1] == "virtual") && (control[4, 2] == "virtual") && (control[4, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[4, 0] == "presencial") && (control[4, 1] == "presencial") && (control[4, 2] == "presencial") && (control[4, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 6
             if ((control[5, 0] == "virtual") && (control[5, 1] == "virtual") && (control[5, 2] == "virtual") && (control[5, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[5, 0] == "presencial") && (control[5, 1] == "presencial") && (control[5, 2] == "presencial") && (control[5, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
             //estudiante 7
             if ((control[6, 0] == "virtual") && (control[6, 1] == "virtual") && (control[6, 2] == "virtual") && (control[6, 3] == "virtual"))
             {
-                control_1 = control_1 + 1;
+                ctrl_vrtl = ctrl_vrtl + 1;
             }
             else if ((control[6, 0] == "presencial") && (control[6, 1] == "presencial") && (control[6, 2] == "presencial") && (control[6, 3] == "presencial"))
             {
-                control_2 = control_2 + 1;
+                ctrl_presencial = ctrl_presencial + 1;
             }
             else
             {
-                control_3 = control_3 + 1;
+                ctrl_hibrid = ctrl_hibrid + 1;
             }
 
-            Console.WriteLine("1: " + control_1);
-            Console.WriteLine("2: " + control_2);
-            Console.WriteLine("3: " + control_3);
+            Console.WriteLine("Numero de estudiantes que recibieron las clases unicamente de manera virtual: " + ctrl_vrtl);
+            Console.WriteLine("Numero de estudiantes que recibieron las clases unicamente de manera presencial: " + ctrl_presencial);
+            Console.WriteLine("Numero de estudiantes que recibieron las clases unicamente de manera hibrida: " + ctrl_hibrid);
         }
     }
 }
